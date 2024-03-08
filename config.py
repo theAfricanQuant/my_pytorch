e5ade60bac41910f2734c651f31b138b0474db86
Ricky Macharm <Ricky.Macharm@SisengAI.com>
2024-03-08 12:57:42 +0100

    new study on pandas 2 & geocode

---
+++ config.py
@@ -0,0 +1,19 @@
+
+# Learning Pytorch
+# in 20234
+# (c) Ricky Macharm, MScFE
+# https://www.SisengAI.com
+#
+#
+import os
+from dotenv import load_dotenv
+
+def set_environment():
+    load_dotenv()  # Load environment variables from .env file
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
+    WOLFRAM_ALPHA_APPID = os.getenv('WOLFRAM_ALPHA_APPID')
+    GOOGLE_KEY = os.getenv('GOOGLE_API_KEY')
+    # You can fetch other keys similarly
+
+set_environment()
