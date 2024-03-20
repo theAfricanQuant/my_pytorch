e2790e563c70d8d5ff7ae2438cd6ed6be7dc93ca
Ricky Macharm <Ricky.Macharm@SisengAI.com>
2024-03-20 20:34:00 +0100

    3rd week of March, 2024

--- config.py
+++ config.py
@@ -13,7 +13,8 @@ def set_environment():
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
     WOLFRAM_ALPHA_APPID = os.getenv('WOLFRAM_ALPHA_APPID')
-    GOOGLE_KEY = os.getenv('GOOGLE_API_KEY')
+    GOOGLE_KEY = os.getenv('GOOGLE_API_KEY') 
+    YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
     # You can fetch other keys similarly
 
 set_environment()
